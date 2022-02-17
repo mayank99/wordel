f374ece11f811da1afa923988277e5a5bde275b8
Mayank <mayank99@users.noreply.github.com>
2022-02-16 19:30:15 -0500

    Initialize repo with vite preset preact-ts
    Also added favicon, prettier and postcss-preset-env

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')({
+      features: {
+        'nesting-rules': true,
+      },
+    }),
+  ],
+}
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import './app.css';
+
+export const App = () => {
+  return (
+    <>
+      <h1>hello wordel</h1>
+    </>
+  );
+};
+
+export default App;
