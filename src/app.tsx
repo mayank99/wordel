e8481901213a50233d05e8467ad92465f6762dad
Mayank <mayank99@users.noreply.github.com>
2022-02-17 00:11:51 -0500

    Implement basic letter checking

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ export const App = () => {
 
   return (
     <>
-      <h1>wordel</h1>
+      <h1>hello wordle</h1>
       <main>
         <Game />
       </main>
@@ -46,7 +46,7 @@ const useGameContext = () => {
 };
 
 const Game = () => {
-  const [answer] = useState('ARSED');
+  const [answer] = useState('arsed');
   const [guesses, setGuesses] = useState<string[]>([]);
 
   return (
@@ -57,21 +57,71 @@ const Game = () => {
 };
 
 const WordsGrid = () => {
+  const { answer, guesses } = useGameContext();
+
   return (
     <div class='WordsGrid'>
       {[...Array(6)].map((_, i) => (
-        <WordGuess key={i}></WordGuess>
+        <WordGuess key={i} guess={guesses[i]} isActive={i === guesses.length && answer !== guesses[i - 1]} />
       ))}
     </div>
   );
 };
 
-const WordGuess = () => {
+const WordGuess = ({ guess, isActive = false }: { guess?: string; isActive?: boolean }) => {
+  const { answer, setGuesses } = useGameContext();
+  const [currentGuess, setCurrentGuess] = useState('');
+  const [activeLetterIndex, setActiveLetterIndex] = useState(0);
+
+  useEffect(() => {
+    if (isActive) {
+      const handleKeyDown = ({ key, altKey, ctrlKey }: KeyboardEvent) => {
+        if (altKey || ctrlKey) {
+          return;
+        }
+        if (key.match(/^[a-z]$/i) && activeLetterIndex < 5) {
+          setCurrentGuess((old) => old + key);
+          setActiveLetterIndex((old) => old + 1);
+        } else if (key === 'Backspace') {
+          setCurrentGuess((old) => old.slice(0, -1));
+          setActiveLetterIndex((old) => Math.max(0, old - 1));
+        } else if (key === 'Enter' && activeLetterIndex === 5) {
+          setGuesses((old) => [...old, currentGuess]);
+          setCurrentGuess('');
+          setActiveLetterIndex(0);
+        }
+      };
+      document.addEventListener('keydown', handleKeyDown);
+      return () => document.removeEventListener('keydown', handleKeyDown);
+    }
+  }, [answer, isActive, activeLetterIndex]);
+
   return (
     <div class='WordGuess'>
-      {[...Array(5)].map((_, i) => (
-        <Letter key={i}>X</Letter>
-      ))}
+      {[...Array(5)].map((_, i) => {
+        const checkLetter = (letter: string) => {
+          if (!guess || !answer) return;
+          const isDuplicateLetter = [...guess].filter((l) => l === letter).length > 1;
+          const hasMultiple = [...answer].filter((l) => l === letter).length > 1;
+
+          return answer[i] === letter
+            ? 'correct'
+            : !answer.includes(letter)
+            ? 'wrong'
+            : !isDuplicateLetter
+            ? 'misplaced'
+            : hasMultiple
+            ? 'misplaced'
+            : 'wrong';
+        };
+
+        const letterState = isActive ? 'active' : !!guess?.[i] ? checkLetter(guess[i]) : 'empty';
+        return (
+          <Letter key={i} state={letterState}>
+            {currentGuess[i] ?? guess?.[i]}
+          </Letter>
+        );
+      })}
     </div>
   );
 };
@@ -80,7 +130,7 @@ const Letter = ({
   state = 'empty',
   children,
   ...rest
-}: { state?: 'empty' | 'wrong' | 'misplaced' | 'correct' } & ComponentProps<'div'>) => {
+}: { state?: 'empty' | 'active' | 'wrong' | 'misplaced' | 'correct' } & ComponentProps<'div'>) => {
   return (
     <div class='Letter' data-state={state} {...rest}>
       <span>{children}</span>
